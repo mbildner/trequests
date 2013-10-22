b7f196cc138e21c3bf442302291b4b89c684b09d
Moshe Bildner <moshe.bildner@gmail.com>
2013-10-22 14:05:08 -0400

    Putting trequests into a class.

--- twisted_requests.py
+++ twisted_requests.py
@@ -2,6 +2,52 @@ from time import time
 import requests
 
 
+		
+
+
+class Trequest(object):
+	def __init__(self, url, callback_function):
+		self.url = url
+		self.callback_function = callback_function
+
+	def __start__(self):
+		r = requests.get(self.url)
+		self.callback_function(r)
+		
+
+		
+
+class TrequestManager(object):
+	def __init__(self):
+		self.holder = []
+		self.responses = []
+
+	def trequest(url, callback_function):
+		t = Trequest(url, callback_function)
+		self.holder.append(t)
+
+	def __continue__(self):
+		if len(self.holder) == len(self.responses):
+			return False
+		else:
+			return True
+
+	def start(self):
+		from twisted.internet import reactor
+		for t in self.holder:
+			reactor.callWhenRunning(t.__start__)
+
+
+
+
+
+
+def report(response):
+	content = response.content
+	print content
+
+
+
 
 languages_list = [
 	"English", 
@@ -16,38 +62,57 @@ languages_list = [
 	"Persian"
 	]
 
-languages_responses = dict()
+tlist = [Trequest("http://en.wikipedia.org/wiki/%s" % l, report) for l in languages_list]
 
-
-
-def keep_running_check(languages_list, languages_responses):
-	if len(languages_list) == len(languages_responses):
-		kill_reactor()
-		print "Kill reactor"
-	else:
-		print "Keep reactor alive"
-		pass
-
-def non_blocking_language_scraper(language, languages_responses):
-	url = 'http://en.wikipedia.org/wiki/%s' % language
-	r = requests.get(url)
-	languages_responses[language] = r
-	keep_running_check(languages_list, languages_responses)
-
-
-def kill_reactor():
-	from twisted.internet import reactor
-	reactor.stop()
 
 from twisted.internet import reactor
 
+for t in tlist:
+	reactor.callWhenRunning(t.start)
 
-for language in languages_list:
-	reactor.callWhenRunning(non_blocking_language_scraper, language, languages_responses)
 
 reactor.run()
 
-print languages_responses
+
+
+
+# languages_responses = dict()
+
+
+
+# def keep_running_check(languages_list, languages_responses):
+# 	if len(languages_list) == len(languages_responses):
+# 		return True
+# 	else:
+# 		return False
+
+# def non_blocking_language_scraper(language, languages_responses):
+# 	url = 'http://en.wikipedia.org/wiki/%s' % language
+# 	r = requests.get(url)
+# 	languages_responses[language] = r
+
+# 	if keep_running_check(languages_list, languages_responses):
+# 		pass
+# 	else:
+# 		kill_reactor()		
+
+
+# def kill_reactor():
+# 	from twisted.internet import reactor
+# 	reactor.stop()
+
+# from twisted.internet import reactor
+
+
+# for language in languages_list:
+# 	reactor.callWhenRunning(non_blocking_language_scraper, language, languages_responses)
+
+
+# start = time()
+# reactor.run()
+# print "took: ", time() - start
+
+# print languages_responses
 
 
 
